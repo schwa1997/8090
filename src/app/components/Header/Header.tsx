c274ab7ff7fd193d3d173cb62e2d3117b1b9d574
huimin chen <huimin.chen@studenti.unipd.it>
2023-10-30 15:01:08 +0100

    add new code

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { FC } from "react";
-import logoLogo from "../../assets/logo.svg";
 import Image from "next/image";
 
 export const Header: FC = () => {
@@ -9,20 +8,19 @@ export const Header: FC = () => {
     <>
       <div className="flex flex-col items-center justify-between w-screen">
         <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-          <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-1/6 lg:bg-none lg:pr-80">
             <a
               className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+              href="/"
               target="_blank"
               rel="noopener noreferrer"
             >
-              {" "}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
                 className="dark:invert"
                 width={100}
-                height={24}
+                height={100}
                 priority
               />
             </a>
@@ -30,18 +28,19 @@ export const Header: FC = () => {
           <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
             <a
               href="/weddingSet"
-              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+              className="group rounded-lg border border-transparent px-5 py-4 transition-colors "
               target="_blank"
               rel="noopener noreferrer"
             >
+              {/* hover:border-gray-300 hover:bg-blue-100 hover:dark:border-neutral-700 hover:dark:bg-blue-100/30 */}
               <h2 className={`mb-3 text-2xl font-semibold`}>
-                婚礼场地布置{" "}
+                婚礼场地布置
                 <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                   -&gt;
                 </span>
               </h2>
               <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                找到属于你的独一无二的私人定制
+                属于你的独一无二的私人定制
               </p>
             </a>
 
@@ -92,7 +91,7 @@ export const Header: FC = () => {
                 </span>
               </h2>
               <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                欢迎添加我们的微信或者到店咨询.
+                欢迎添加我们的微信或者到店咨询
               </p>
             </a>
           </div>
